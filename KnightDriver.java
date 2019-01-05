7bbb4c2683048e6b7d9a64443a22cef99d92acaa
Anshul Birla <anshulbirla@gmail.com>
2019-01-04 21:32:32 -0800

    main class structure finished

--- KnightDriver.java
+++ KnightDriver.java
@@ -29,13 +29,18 @@ public class KnightDriver
             {
                 set();
                 move();
+                for (int x = 1; x< moveCheck.length(); x++)
+                {
+                    moveCheck[x] = 0;
+                }
             }
-            el
-            //if it works, give boolean true 
-            //if the previous boolean is still false, and
-               //all numbers have been exhausted, give the main while 
-               //loop boolean false
+            else
+            {
+                if(checkMoveNum == 9)
+                    mainCheck = false
+                }
             
-            
+            }
     }
 }
+
