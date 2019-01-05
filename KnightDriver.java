73c6b1ab1025559f491ae1f1e6f658139b5017e2
Anshul Birla <anshulbirla@gmail.com>
2019-01-04 22:41:18 -0800

    commenting in driver

--- KnightDriver.java
+++ KnightDriver.java
@@ -10,9 +10,11 @@ public class KnightDriver
 {
     public static void main(String [] args)
     {
+        //constructors
         Random randy = new Random();
         KnightsTour knighty = new KnightsTour();
         
+        //initialize
         boolean mainCheck = true;
         boolean works = false;
         int[] moveCheck = new int [9];
@@ -21,33 +23,43 @@ public class KnightDriver
        
         moveCheck[0] = 1;
         
+        //master loop
           while(mainCheck)
         {
+            //generates random integer
             number = randy.nextInt(8)+1;
+            
+            //checks if the move works 
             works = knighty.checkMove(number);
             
+            //record the number has been used in this iteration
             moveCheck[number] = 1;
             
             if (works)
             {
+                //both work in tandem to move the knight
                 knighty.set();
                 knighty.move(counter);
                 
+                //flushes the moveCheck array
                 for (int x = 1; x< moveCheck.length; x++)
                 {
                     moveCheck[x] = 0;
                 }
                 
+                //increment counter
                 counter++;
             }
             else
             {
+                //if all numbers have been used, terminate the while loop
                 if(knighty.checkMoveNum(moveCheck) == 9)
                     mainCheck = false;
-                }
+             }
             
             }
         
+        //ouput
         knighty.print(counter);
     }
 }
